7a0a9222cf7b6209f5969313e8578d90a4cf4a44
TheToBePro <zoolcool839@gmail.com>
2023-04-01 00:13:48 -0700

    execries soultions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(number1,number2) {
+    if (!Number.isInteger(number1) || !Number.isInteger(number2)) return "ERROR";
+    if (number1<0||number2<0) {
+        return "ERROR";
+    }
+    let bigNumber = 0;
+    if (number1>number2) {
+        bigNumber += number1;
+    }else{
+        bigNumber += number2;
+    }
+    let smallNumber = 0;
+    if (number1<number2) {
+        smallNumber += number1;
+    }else{
+        smallNumber += number2;
+    }
+    let finalSum = 0;
+    for (let i = smallNumber; i < bigNumber + 1; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
-
+console.log(sumAll(4,1));
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+    let determineLeapYear;
+    if (leapYear % 4 === 0 && leapYear % 100 !== 0 ) {
+         determineLeapYear = true;
+    }else if(leapYear % 400 === 0) {
+        determineLeapYear = true;
+    }else{
+        determineLeapYear = false;
+    }
+    return determineLeapYear;
 };
 
 // Do not edit below this line

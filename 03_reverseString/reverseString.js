26a2be24c5e010b4104eaee894de0de7c467648e
TheToBePro <zoolcool839@gmail.com>
2023-03-31 22:43:28 -0700

    execries solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordSplit = word.split('');
+    let wordReverse = wordSplit.reverse();
+    let wordJoin = wordReverse.join('');
+    return wordJoin;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+// a function get two arguments to remove array element and deal with multiple argument options
+const removeFromArray = function(myarray, ...theDeletedElements) {
+    const Newitems = [];
+    myarray.forEach((item) => {
+        if (!theDeletedElements.includes(item)) {
+            Newitems.push(item);
+          }
+    });
+        return Newitems;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
